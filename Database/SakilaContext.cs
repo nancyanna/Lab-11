e1a2c9af25b1cce3313349da4e5a69c4c57a17ac
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-03-29 12:15:23 -0500

    Add files via upload

---
+++ Database/SakilaContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+
+namespace Database
+{
+    class SakilaContext : DbContext
+    {
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;");
+
+        }
+    }
+}
